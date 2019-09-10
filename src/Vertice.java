e259c1d37a621d219941a94bc34a054d877bdd00
Ryan <ryandf3602@gmail.com>
2019-09-10 17:04:18 -0300

    Geracao de Filhos

--- src/Vertice.java
+++ src/Vertice.java
@@ -1,16 +1,17 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Vertice {
     
     private Tabuleiro tabuleiroEstado;
-    private List<Vertice> verticesAdjacentes = new ArrayList<Vertice>();
+    private Set<Vertice> verticesAdjacentes = new HashSet<Vertice>();
     private Vertice verticePai;
     private String jogadaText; // saber se é X ou O
     
     public String getJogadaText() {
         return jogadaText;
     }
+    
     public void setJogadaText(String jogadaText) {
         this.jogadaText = jogadaText;
     }
@@ -18,17 +19,21 @@ public class Vertice {
     public Vertice getVerticePai() {
         return verticePai;
     }
+    
     public void setVerticePai(Vertice verticePai){
         this.verticePai = verticePai;
     }
     
+    public void setTabuleiroEstado(Tabuleiro tabuleiroEstado){
+        this.tabuleiroEstado = tabuleiroEstado;
+    } 
+    
     public Tabuleiro getTabuleiroEstado() {
         return tabuleiroEstado;
     }
+    
     //Add vertices Filhos a Vértice atual
     public void AdicionarVertice(Vertice vertice) {
-        verticesAdjacentes.add(vertice);
-        
-        
+        verticesAdjacentes.add(vertice);  
     }
 }
